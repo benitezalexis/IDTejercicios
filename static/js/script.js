dbffeab0fe3b0388258265454f842816ef5dd113
benitezalexis <abenitezcolman96@gmail.com>
2023-05-06 21:45:36 -0400

    Ejercicio clase 9

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
+
+
+    $('#ejerNueve').click(function(){
+        $(location).attr('href','./ejercicio2.html');
+    });
+
+    $('#ejerNueve1').click(function(){
+        $(location).attr('href','./ejercicio1.html');
+    });
+
     $('#btnAtras').click(function(){
         $(location).attr('href','../index.html');
     });
+
 });
